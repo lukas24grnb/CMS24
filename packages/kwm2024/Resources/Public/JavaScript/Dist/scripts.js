9f6a30e6a6aa039f90228613403a6e88830fd311
Lukas Grünberger <s2310456003@fhooe.at>
2024-12-13 11:42:44 +0100

    Initial commit

---
+++ packages/kwm2024/Resources/Public/JavaScript/Dist/scripts.js
@@ -0,0 +1,10 @@
+/*!
+ * KWM 2024 v1.0.0 (https://www.fh-ooe.at)
+ * Copyright 2017-2024 Lukas Grünberger
+ * Licensed under the GPL-2.0-or-later license
+ */
+console.log("WE LOVE TYPO3");
+
+document.querySelector("#meinButton1").addEventListener("click", function (){
+    alert("Button clicked");
+});
